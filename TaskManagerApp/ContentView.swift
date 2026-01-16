3d7e7888582c9d9cfb90543ae2efbaebb5ae8e21
Shugyla <rafikovashugyla@gmail.com>
2026-01-16 23:10:04 +0500

    part 2 task 2

--- TaskManagerApp/ContentView.swift
+++ TaskManagerApp/ContentView.swift
@@ -21,14 +21,25 @@ struct ContentView: View {
         NavigationStack {
             VStack(spacing: 16) {
                 
-                Text("Tasks count: \(tasks.count)")
+                VStack(alignment: .leading, spacing: 4) {
+                        Text("Task Manager")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+
+                        Text("Active tasks: \(tasks.filter { !$0.isCompleted }.count)")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal)
+                    .padding(.top, 8)
                 
                 Button("Add Task") {
                     tasks.append(
                         ToDoItem(title: "New Task", priority: "Low", isCompleted: false)
                     )
                 }
-                
+                Spacer()
             }
             .padding()
             .navigationTitle("My Tasks")
